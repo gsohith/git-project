eef225250d21f38817b74547fff6adb9248b1c4e
sohith royal <gksohith@gmail.com>
2020-09-04 11:40:28 +0530

    first commit of hello world

---
+++ hello_world.c
@@ -0,0 +1,12 @@
+/*Simple helo world program
+	@Author : sohith
+	@Date : 3-9-2020
+	@Dexcription : Hello world program
+*/
+
+#include <stdio.h>
+int main()
+{
+	printf("Hello world\n");
+	return 0;
+}
